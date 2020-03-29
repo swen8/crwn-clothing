f3063a55adcfa38a87042d6a3278907c0c9bc6ed
unknown <swen.koebler12@googlemail.com>
2020-03-29 10:47:19 +0200

    created cartitem component and connected our cart with cartitem reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss'
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 const CartIcon = ( {toggleCartHidden} ) => (
     <div className='cart-icon' onClick={toggleCartHidden}> 
