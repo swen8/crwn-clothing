b16654a803f1285c2c640f28be8fa14048b9aac0
unknown <swen.koebler12@googlemail.com>
2020-04-05 11:20:25 +0200

    moved our directory and shop data into its own reducer, created respective selectors an updating corresponding with new redux flow for directory/shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware } from 'redux';
+import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlerwares = [logger];
 
-const store =createStore(rootReducer, applyMiddleware(...middlerwares))
+export const store =createStore(rootReducer, applyMiddleware(...middlerwares))
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
